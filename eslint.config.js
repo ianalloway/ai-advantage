ddde3eb97f42a9223694cdd1866cd6a8e63146f8
Ian Alloway <adapter_burners.1y@icloud.com>
2026-09-03 13:44:22 -0400

    chore: upgrade TypeScript to 7.0.2 and drop typescript-eslint blocker

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,26 @@
 import js from "@eslint/js";
+import babelParser from "@babel/eslint-parser";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default [
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        sourceType: "module",
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          presets: [["@babel/preset-typescript", { allExtensions: true, isTSX: true }]],
+        },
+      },
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: globals.browser,
     },
     plugins: {
@@ -18,14 +28,16 @@ export default tseslint.config(
       "react-refresh": reactRefresh,
     },
     rules: {
+      ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      "no-undef": "off",
       "react-hooks/set-state-in-effect": "off",
       "react-hooks/purity": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrors: "none" }],
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrors: "none" }],
     },
-  }
-);
+  },
+];
